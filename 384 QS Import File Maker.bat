6e69b9f220530c58921eefbdfc3baa8f9604e350
Fillpalm <31626166+Fillpalm@users.noreply.github.com>
2021-12-23 15:00:59 -0500

    Add files via upload

---
+++ 384 QS Import File Maker.bat
@@ -0,0 +1,3 @@
+@echo off
+call conda activate
+call python D:\Users\INSTR-ADMIN\Documents\384reformat\gui_test.py
---
+++ 96 QS Import File Maker.bat
@@ -0,0 +1,3 @@
+@echo off
+call conda activate
+call python D:\Users\INSTR-ADMIN\Documents\96reformat\96to96r.py
